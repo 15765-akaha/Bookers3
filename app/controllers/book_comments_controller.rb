92d58716e8f6454cf2f02985fda47c06a33c533f
15765-akaha <turning2012@gmail.com>
2020-02-09 10:11:49 +0000

    　コメント削除機能追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
-  	book = Book.find(params[:book_id])
-  	comment = BookComment.new(book_comment_params)
-  	comment.user_id = current_user.id
-  	comment.book_id = book.id
-  	comment.save
-  	redirect_back(fallback_location: root_path)
+  	@book = Book.find(params[:book_id])
+  	@comment = BookComment.new(book_comment_params)
+  	@comment.user_id = current_user.id
+  	@comment.book_id = @book.id
+  	if @comment.save
+  	   redirect_back(fallback_location: root_path)
+    else
+       redirect_to book_path(@book)
+    end
   end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:book_id])
+    @comment.destroy
+    redirect_to book_path(@book)
 
   private
    def book_comment_params
