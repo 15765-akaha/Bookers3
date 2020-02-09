c5ceb2c1995f8445cded2e3057da7424aeb0a823
15765-akaha <turning2012@gmail.com>
2020-02-09 14:40:45 +0000

    アクセス制限

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,5 +1,6 @@
 class BookCommentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :ensure_correct_user, {only: [:destroy]}
   def create
   	@book = Book.find(params[:book_id])
   	@comment = BookComment.new(book_comment_params)
@@ -23,5 +24,11 @@ class BookCommentsController < ApplicationController
    def book_comment_params
    		params.require(:book_comment).permit(:user_id,:book_id,:comment)
    end
+   def ensure_correct_user
+    @comment = BookComment.find(params[:id])
+    if current_user.id != @comment.user_id
+       redirect_to books_path
+    end
+  end
 end
 
