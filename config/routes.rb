ded4301f7020a1e800dbe820abe0e36a73c4721d
15765-akaha <turning2012@gmail.com>
2020-02-09 06:49:50 +0000

    いいね件数表示改善

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'book_comment/create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home/index'
@@ -7,6 +6,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :books do
   resource :favorites, only: [:create, :destroy]
+  resource :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :index]
 end
