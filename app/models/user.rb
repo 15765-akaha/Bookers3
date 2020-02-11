a247bada525beeac20b67d9b43f24a7751174669
15765-akaha <turning2012@gmail.com>
2020-02-11 03:43:03 +0000

    アソシエーション経路作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,32 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
-  has_many :followings, through: :following_relationships
+  # フォロー側の視点
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :follower
+  # フォローされる側の視点
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :following
 
-  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
-  has_many :followers, through: :follower_relationships
-  
   attachment :profile_image
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
-  def following?(other_user)
-    following_relationships.find_by(following_id: other_user.id)
+
+  def followed_by?(user)
+    # 今自分(引数のuser)がフォローしようとしているユーザー(レシーバー)がフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる
+    passive_relationships.find_by(following_id: user.id).present?
   end
 
-  def follow!(other_user)
-    following_relationships.create!(following_id: other_user.id)
-  end
+#   def following?(other_user)
+#     following_relationships.find_by(following_id: other_user.id)
+#   end
 
-  def unfollow!(other_user)
-    following_relationships.find_by(following_id: other_user.id).destroy
-  end
-end
+#   def follow!(other_user)
+#     following_relationships.create!(following_id: other_user.id)
+#   end
+
+#   def unfollow!(other_user)
+#     following_relationships.find_by(following_id: other_user.id).destroy
+#   end
+# end
